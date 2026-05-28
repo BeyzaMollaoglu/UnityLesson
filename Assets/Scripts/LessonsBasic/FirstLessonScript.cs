0000ec64bd11c29727b20ac5d0c86d5b25730e9f
Beyzanur Mollaoğlu <byzmllogluu@gmail.com>
2026-05-28 15:04:47 +0300

    create scriptable object

--- Assets/Scripts/LessonsBasic/FirstLessonScript.cs
+++ Assets/Scripts/LessonsBasic/FirstLessonScript.cs
@@ -3,13 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace LessonsBasic
-{
+{   ///
+
+
     public class FirstLessonScript : MonoBehaviour
     {
+        private float ondalikSayi = 0.25f;
+        [SerializeField] private Vector3 ucFloat = new Vector3(1, 2, 3);
+
+        public float floatProperty { get; set; } // kendimiz yonetiriz geti ya da seti private olarak ayirabiliriz,,,,, 
+
         // Start is called before the first frame update
         void Start()
         {
-
+            ondalikSayi = 10000;
+            floatProperty = 100000f;
         }
 
         // Update is called once per frame
